d13808f03b1191fd2016c9c711ac5818f4ba72c1
victor.rubem <victor.rubem@radixeng.com.br>
2020-03-11 17:20:26 -0300

    Carregador de módulos, loader, servidor local

--- app/js/models/Negociacao.js
+++ app/js/models/Negociacao.js
@@ -1,22 +1,33 @@
-class Negociacao {
-    constructor(_data, _quantidade, _valor) {
-        this._data = _data;
-        this._quantidade = _quantidade;
-        this._valor = _valor;
-    }
-    dataFormatada() {
-        return `${this._data.getDate()}/${this._data.getMonth()}/${this._data.getFullYear()}`;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
-    }
-    get volume() {
-        return this.quantidade * this._valor;
-    }
-}
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var Negociacao;
+    return {
+        setters: [],
+        execute: function () {
+            Negociacao = class Negociacao {
+                constructor(_data, _quantidade, _valor) {
+                    this._data = _data;
+                    this._quantidade = _quantidade;
+                    this._valor = _valor;
+                }
+                dataFormatada() {
+                    return `${this._data.getDate()}/${this._data.getMonth()}/${this._data.getFullYear()}`;
+                }
+                get data() {
+                    return this._data;
+                }
+                get quantidade() {
+                    return this._quantidade;
+                }
+                get valor() {
+                    return this._valor;
+                }
+                get volume() {
+                    return this.quantidade * this._valor;
+                }
+            };
+            exports_1("Negociacao", Negociacao);
+        }
+    };
+});
